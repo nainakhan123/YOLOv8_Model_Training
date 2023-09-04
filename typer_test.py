f05c56f91b96c8f515e1694c60d655bb3655909e
nainakhan123 <109538331+nainakhan123@users.noreply.github.com>
2023-09-04 10:04:15 +0500

    Add files via upload

---
+++ typer_test.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def test_yolo(
+    weights_path: str = typer.Argument(),
+    test_path: str = typer.Argument(),
+):
+    print("Testing started")
+    model = YOLO(weights_path)
+    model.predict(test_path, save=True)
+    print("Testing done successfully")
+
+if __name__ == "__main__":
+    app()
