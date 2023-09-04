4a48f6b2427d25c09a22bb098412e94d878661ff
Sumbal Khan <sl.imran891@gmail.com>
2023-09-04 12:08:08 +0500

    Merged _CLI files into main files

--- train.py
+++ train.py
@@ -1,6 +1,21 @@
+import argparse
 from ultralytics import YOLO
 
-model = YOLO('/home/sumbalkhan12/Test/barcode-detecion/runs/detect/train5/weights/best.pt')
-model.train(data="/home/sumbalkhan12/Test/barcode-detecion/large_dataset/custom.yaml", epochs=2)
-print('Successfull2') 
-model.val()
+def train_yolo(weights_path, data_config, epochs):
+    print('Starting training...')
+    model = YOLO(weights_path)
+    model.train(data=data_config, epochs=epochs)
+    print('Training completed.')
+
+def main():
+    parser = argparse.ArgumentParser(description='Train YOLOv8 on custom data')
+    parser.add_argument('weights', required=True)
+    parser.add_argument('data-config', required=True)
+    parser.add_argument('epochs', type=int, default=2)
+    
+    args = parser.parse_args()
+    
+    train_yolo(args.weights, args.data_config, args.epochs)
+
+if __name__ == '__main__':
+    main()
