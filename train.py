4e21f536fa1e05112f17c747a723b3ddef6e11b1
Sumbal Khan <sl.imran891@gmail.com>
2023-09-06 09:44:44 +0500

    Code is replaced with the code of Typer library

--- train.py
+++ train.py
@@ -1,6 +1,20 @@
+import typer
 from ultralytics import YOLO
 
-model = YOLO('/home/sumbalkhan12/Test/barcode-detecion/runs/detect/train5/weights/best.pt')
-model.train(data="/home/sumbalkhan12/Test/barcode-detecion/large_dataset/custom.yaml", epochs=2)
-print('Successfull2') 
-model.val()
+app = typer.Typer()
+
+@app.command()
+def train_yolo(
+    model_path: str = typer.Argument(),
+    data_path: str = typer.Argument(),
+    epochs: int = typer.Option(),
+):
+    print('start')
+    model = YOLO(model_path)
+    print('Successfully loaded YOLO model')
+    model.train(data=data_path, epochs=epochs)
+    print('Successfully trained YOLO model')
+    model.val()
+
+# if __name__ == "__main__":
+#     app()
