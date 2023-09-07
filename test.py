1c7fe8c121251b70c315c842e7ff5a1ac4b192a3
Sumbal Khan <sl.imran891@gmail.com>
2023-09-07 15:58:23 +0500

    Empty lines and validate function added

--- test.py
+++ test.py
@@ -1,16 +1,20 @@
 from ultralytics import YOLO
 import argparse
 
+
 def predict_yolo(path_to_weights, path_to_test):
     model = YOLO(path_to_weights)
     model.predict(source=path_to_test)
+
 
 def main():
     parser = argparse.ArgumentParser()  
     parser.add_argument('path_to_weights')
     parser.add_argument('path_to_test')
     
+
     args = parser.parse_args()
+
 
     predict_yolo(args.path_to_weights, args.path_to_test)
 
