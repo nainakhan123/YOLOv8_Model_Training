0f6c736fed13f764bbcdabde0c0cadc2febf891b
Sumbal Khan <sl.imran891@gmail.com>
2023-09-20 11:36:47 +0500

    Variables name updated

--- test.py
+++ test.py
@@ -8,11 +8,11 @@ app = typer.Typer()
 @app.command()
 def test_yolo(
     path_to_best_weights: str = typer.Argument(),
-    path_to_test_file: str = typer.Argument(),
+    path_to_test_dataset: str = typer.Argument(),
 ):
     print("Testing started")
     model: YOLO = YOLO(path_to_best_weights)
-    model.predict(path_to_test_file, save=True)
+    model.predict(path_to_test_dataset, save=True)
     print("Testing done successfully")
 
 
--- train.py
+++ train.py
@@ -7,14 +7,14 @@ app = typer.Typer()
 
 @app.command()
 def train_yolo(
-    pre_trained_model_path: str = typer.Argument(),
-    data_to_be_trained_path: str = typer.Argument(),
+    pre_trained_model_config_path: str = typer.Argument(),
+    dataset_to_be_trained_path: str = typer.Argument(),
     epochs: int = typer.Option(),
 ):
     print("start")
-    model: YOLO = YOLO(pre_trained_model_path)
+    model: YOLO = YOLO(pre_trained_model_config_path)
     print("Successfully loaded YOLO model")
-    model.train(data=data_to_be_trained_path, epochs=epochs)
+    model.train(data=dataset_to_be_trained_path, epochs=epochs)
     print("Successfully trained YOLO model")
     model.val()
 
