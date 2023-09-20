1af93de34e44a195feef33028727ae3607cc873d
Sumbal Khan <sl.imran891@gmail.com>
2023-09-20 14:28:20 +0500

    Positioned variables changed to name variable

--- cli.py
+++ cli.py
@@ -8,19 +8,27 @@ app = typer.Typer()
 
 @app.command()
 def train(
-    pre_trained_model_path: str = typer.Argument(),
-    data_to_be_trained_path: str = typer.Argument(),
-    epochs: int = typer.Option(),
+    pre_trained_model_path: str = typer.Option(
+        "--path_to_pretrained_model", help="Path to pre-trained model"
+    ),
+    data_to_be_trained_path: str = typer.Option(
+        "--path_to_dataset_to_be_trained", help="Path to dataset to be train"
+    ),
+    epochs: int = typer.Option("--epochs", help="Number of epochs"),
 ):
     train_yolo(pre_trained_model_path, data_to_be_trained_path, epochs)
 
 
 @app.command()
 def test(
-    path_to_best_weights: str = typer.Argument(),
-    path_to_test_file: str = typer.Argument(),
+    path_to_best_weights: str = typer.Option(
+        "--best_weight_path", help="Path to weights generated after training"
+    ),
+    path_to_test_data: str = typer.Option(
+        "--test_data_path", help="Path to test dataset to the test the performance"
+    ),
 ):
-    test_yolo(path_to_best_weights, path_to_test_file)
+    test_yolo(path_to_best_weights, path_to_test_data)
 
 
 if __name__ == "__main__":
