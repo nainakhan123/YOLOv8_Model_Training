fce1fbc4f4eec02a43f9b5e9ea89c370e01a40ee
Sumbal Khan <sl.imran891@gmail.com>
2023-09-22 11:36:30 +0500

    Api developed using fastapi

---
+++ api.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+from test import test_yolo
+from train import train_yolo
+
+
+app = FastAPI()
+
+
+class TrainRequest(BaseModel):
+    pre_trained_model_path: str
+    dataset_config_path: str
+    epochs: int
+
+
+class TestRequest(BaseModel):
+    path_to_best_weights: str
+    path_to_test_data: str
+
+
+@app.post("/train/")
+def train_model(train_data: TrainRequest):
+    try:
+        train_yolo(
+            train_data.pre_trained_model_path,
+            train_data.dataset_config_path,
+            train_data.epochs,
+        )
+        return {"message": "Training completed successfully"}
+    except Exception as exp:
+        raise HTTPException(
+            status_code=500, detail=f"Error during training: {str(exp)}"
+        ) from exp
+
+
+@app.post("/test/")
+def test_model(test_data: TestRequest):
+    try:
+        test_yolo(test_data.path_to_best_weights, test_data.path_to_test_data)
+        return {"message": "Testing completed successfully"}
+    except Exception as exp:
+        raise HTTPException(
+            status_code=500, detail=f"Error during testing: {str(exp)}"
+        ) from exp
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=9000)
