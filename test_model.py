2cdb50db8bfd18130098e4fe1ee7cb3cb2617f49
Sumbal Khan <sl.imran891@gmail.com>
2023-09-22 16:21:41 +0500

    Extra spaces removed

--- test_model.py
+++ test_model.py
@@ -6,3 +6,4 @@ def test_yolo(path_to_best_weights: str, path_to_test_data: str):
     model: YOLO = YOLO(path_to_best_weights)
     model.predict(path_to_test_data, save=True)
     print("Testing done successfully")
+    
