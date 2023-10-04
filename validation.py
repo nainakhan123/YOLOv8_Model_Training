c53228c79b8a126c1c0fd5d131f8f07c2ab5371b
Sumbal Khan <sl.imran891@gmail.com>
2023-10-04 15:44:52 +0500

    test_model name changed to validation

---
+++ validation.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+
+def validation_yolo(path_to_best_weights: str, path_to_test_data: str):
+    print("Testing started")
+    model: YOLO = YOLO(path_to_best_weights)
+    model.predict(path_to_test_data, save=True)
+    print("Testing done successfully")
+    return "Testing Successful"
