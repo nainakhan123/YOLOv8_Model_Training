0d2e17d4d77e48f3287a6f5c3714debe769a1e81
Sumbal Khan <sl.imran891@gmail.com>
2023-10-04 15:40:13 +0500

    test_model renamned to validation

--- test_model.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-
-def test_yolo(path_to_best_weights: str, path_to_test_data: str):
-    print("Testing started")
-    model: YOLO = YOLO(path_to_best_weights)
-    model.predict(path_to_test_data, save=True)
-    print("Testing done successfully") 
