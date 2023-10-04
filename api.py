7873b74fe42f13481e1e1afea6ab89112625e7db
Sumbal Khan <sl.imran891@gmail.com>
2023-10-04 10:48:38 +0500

    Hard coded weights path

--- api.py
+++ api.py
@@ -1,17 +1,16 @@
+import os
 from fastapi import FastAPI, HTTPException, File, UploadFile
 from ultralytics import YOLO
 import uvicorn
-import os
 
-from models import (
-    TestRequest,
-    TrainRequest
-)
+from models import TrainRequest
 
-from test_model import test_yolo
+
 from train_model import train_yolo
 
+
 app = FastAPI()
+
 
 @app.post("/train_model/")
 async def train_model(train_data: TrainRequest):
@@ -27,32 +26,38 @@ async def train_model(train_data: TrainRequest):
             status_code=500, detail=f"Error during training: {str(exp)}"
         ) from exp
 
+
+# best_weights_path = "/home/sumbalkhan12/YOLOv8_Model_Training/runs/detect/train2/weights/best.pt"
+
+
 @app.post("/test_model/")
 async def test_model(
-    path_to_best_weights: UploadFile = File(...),
     path_to_test_data: UploadFile = File(...),
+    best_weights_path="/home/sumbalkhan12/Test/barcode-detecion/runs/detect/train2/weights/best.pt",
 ):
     try:
-        best_weights_path:str = os.path.join("/tmp", path_to_best_weights.filename)
-        test_data_path:str = os.path.join("/tmp", path_to_test_data.filename)
+        test_data_path: str = os.path.join("/tmp", path_to_test_data.filename)
 
-        with open(best_weights_path, "wb") as best_weights_file:
-            best_weights_file.write(path_to_best_weights.file.read())
         with open(test_data_path, "wb") as test_data_file:
             test_data_file.write(path_to_test_data.file.read())
+            print("picture uploaded")
 
-        model:YOLO = YOLO(best_weights_path)
-        results:str = model(test_data_path)
-
-        bounding_boxes:int = []
+        model = YOLO(best_weights_path)
+        results = model(test_data_path)
+        print("Results")
+        bounding_boxes = []
         for result in results:
             bounding_boxes.extend(result.boxes.xyxy.tolist())
 
-        return {"bounding_boxes": bounding_boxes, "message": "Testing done successfully"}
+        return {
+            "bounding_boxes": bounding_boxes,
+            "message": "Testing done successfully",
+        }
     except Exception as exp:
         raise HTTPException(
             status_code=500, detail=f"Error during testing: {str(exp)}"
         ) from exp
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=9000)
